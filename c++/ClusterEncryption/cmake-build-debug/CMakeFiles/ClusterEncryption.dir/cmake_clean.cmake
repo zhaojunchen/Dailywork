62f60197cea34bfa0d423d7067e72da8966b7e9c
zhaojunchen <41560016+zhaojunchen@users.noreply.github.com>
2020-03-09 19:16:52 +0800

    新建文件簇加密项目
    配置错误、已删除

---
+++ c++/ClusterEncryption/cmake-build-debug/CMakeFiles/ClusterEncryption.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClusterEncryption.dir/main.cpp.obj"
+  "ClusterEncryption.exe"
+  "ClusterEncryption.exe.manifest"
+  "ClusterEncryption.pdb"
+  "libClusterEncryption.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClusterEncryption.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
